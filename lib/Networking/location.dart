b16723954add974dc0b41fd95a970a2e1e1a3705
vvs-personalstash <viralkverma0987@gmail.com>
2023-08-24 04:47:19 +0530

    changes in location

--- lib/Networking/location.dart
+++ lib/Networking/location.dart
@@ -1,21 +1,44 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 class Location with ChangeNotifier {
   double? latitudeoflocation;
   double? longitudeoflocation;
+  // Future<void> getPermission() async {
+  //   try {
+
+  //     print(permission);
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
 
   Future<void> getCurrentLocation() async {
     try {
+      bool serviceEnabled;
       LocationPermission permission;
-      permission = await Geolocator.requestPermission();
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+      permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied) {
-        print('locaation permission denied');
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
       }
 
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      print('lmao');
       Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
+          desiredAccuracy: LocationAccuracy.high);
+      print('location');
+      print(position.latitude);
       latitudeoflocation = position.latitude;
       longitudeoflocation = position.longitude;
     } catch (e) {
